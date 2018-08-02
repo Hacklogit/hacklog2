41f1fc5ad731be70edd5b0baf0f8bf2b26cfb12e
Stefano Novelli <murdercode@gmail.com>
2018-08-02 12:40:36 +0200

    update

---
+++ examples/chapter7/3.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET['signature']) {
+	$signature_user = $_GET['signature'];
+
+	// Qui una query che salva il valore all'interno di un database
+
+}
+
+// Qui una query che recupera la firma all'interno della variabile $row
+
+echo $row["signature"];
+?>
